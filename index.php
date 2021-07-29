0140ba26bcb7175dbcfb155d3bf81c1cc5e53609
Mary Carmen Huamani Martinez <201615146h@utea.edu.pe>
2021-07-29 10:58:04 -0500

    estilo

--- index.php
+++ index.php
@@ -5,18 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="sty.css">
     <title>TodoList</title>
     <script src="https://kit.fontawesome.com/a5bc61b517.js" crossorigin="anonymous"></script>
+    
 </head>
 <style>
 
 </style>
 
 <body  >
+  <h1>Tareas disponibles</h1>
   <div class="login-continer">
       <img src="720.jpg" alt="">
-      <p>Sistema login</p>
+      <p>Iniciar tareas</p>
       <div class="fields">
          <div class="data">
          <i class="fas fa-at"></i>
@@ -24,13 +26,13 @@
          </div>  
          <div class="data">
          <i class="fas fa-lock"></i>
-         <input type="Contraseña" placeholder="Contraseña">
+         <input type="password" name="contraseña" placeholder="Contraseña">
 
          </div>
       </div>
      
-      
-   <button class="btm-login">Entra</button>
+      <button type class="btn-login">LOGIN</button>
+      <p> <a href="registrarvista.php">REGISTRATE</a></p>
   </div>
     
 </body>
