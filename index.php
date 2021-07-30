7d8e0291358b8f0569ed70e8c81a7ecc50718cbd
Mary Carmen Huamani Martinez <201615146h@utea.edu.pe>
2021-07-30 00:45:39 -0500

    html y ccs

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="sty.css">
+    <link rel="stylesheet" href="styles.css">
     <title>TodoList</title>
     <script src="https://kit.fontawesome.com/a5bc61b517.js" crossorigin="anonymous"></script>
     
@@ -15,25 +15,27 @@
 </style>
 
 <body  >
+<form action="validar.php" method="post">
   <h1>Tareas disponibles</h1>
-  <div class="login-continer">
+
+  <div class="login-continer" >
       <img src="720.jpg" alt="">
       <p>Iniciar tareas</p>
+      <label for="">CORREO ELECTRONICO</label>
       <div class="fields">
          <div class="data">
          <i class="fas fa-at"></i>
-         <input type="Ingresa tu correo" placeholder="Usuario">
+         <input type="text" name="correo" placeholder="Correo electronico" required>
          </div>  
+         <label for="">CONTRASEÑA</label>
          <div class="data">
          <i class="fas fa-lock"></i>
-         <input type="password" name="contraseña" placeholder="Contraseña">
-
+         <input type="password" name="contraseña" placeholder="Contraseña" required>
          </div>
       </div>
-     
-      <button type class="btn-login">LOGIN</button>
-      <p> <a href="registrarvista.php">REGISTRATE</a></p>
+      <button type type="submit" class="btn-login">LOGIN</button> <a class="button" href="registrarvista.php">REGISTRATE</a>
   </div>
+</form>
     
 </body>
 
